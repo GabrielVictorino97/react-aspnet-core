8cc45c7e6074b06f40fcc0ed225bd62502b28790
Gabriel Victorino <gavictorino97@gmail.com>
2022-09-02 10:30:45 -0300

    add react project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import {useState} from 'react';
+
+let initialState = [
+  {
+    id: "1",
+    descricao: "Primeira atividade",
+  },
+  {
+    id: "2",
+    descricao: "Segunda atividade",
+  }
+];
 
 function App() {
+  const [atividades, setAtividades] = useState(initialState)
+  
+  function addAtividade(e){
+    e.preventDefault();
+
+    const atividade ={
+      id: document.getElementById('id').value,
+      descricao: document.getElementById('descricao').value,
+    };
+    
+    setAtividades([...atividades, {... atividade}]);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <form>
+    <input id="id" type="text" placeholder="id"></input>
+    <input id="descricao" type="text" placeholder="descricao"></input>
+    <button onClick={addAtividade}>+ Atividade</button>
+    </form>
+    <div className="mt-3">
+      <ul className="list-group">
+        {atividades.map((ativ) => (
+          <li key={ativ.id} className="list-group-item">{ativ.id} - {ativ.descricao} </li>
+        ))}
+      </ul>
     </div>
+    </>
   );
 }
 
